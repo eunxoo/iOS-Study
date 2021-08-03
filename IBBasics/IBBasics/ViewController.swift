3f69737b5193fac55bbece066e5357ed7cbeff7d
eunxoo <80334038+eunxoo@users.noreply.github.com>
2021-08-03 23:14:47 +0900

    IBBasics

---
+++ IBBasics/IBBasics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBBasics
+//
+//  Created by 양은수 on 2021/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
