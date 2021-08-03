203589d74f2db4bcd5dc3028ed5ad2d382645b26
eunxoo <80334038+eunxoo@users.noreply.github.com>
2021-08-03 23:47:55 +0900

    update

--- IBBasics/IBBasics/ViewController.swift
+++ IBBasics/IBBasics/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myButton.setTitleColor(.red, for: .normal)
     }
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("The button was pressed")
+    }
+    
 
 }
 
